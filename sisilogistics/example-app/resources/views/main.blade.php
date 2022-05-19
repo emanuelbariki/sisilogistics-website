a46625c94c8db1a42db038c65c7363577ff6f2bc
mashely <ansherotimalnado10@gmail.com>
2022-05-19 12:43:35 +0300

    remove of  dot

--- sisilogistics/example-app/resources/views/main.blade.php
+++ sisilogistics/example-app/resources/views/main.blade.php
@@ -201,7 +201,7 @@
       <div class="row">
         <div class="col-lg-12">
           <p class="footer-text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This Website is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://creative.sisilimited.co.tz" target="_blank">SISI Creative. </a>. 
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This Website is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://creative.sisilimited.co.tz" target="_blank">SISI Creative. </a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
         </div>
       </div>
