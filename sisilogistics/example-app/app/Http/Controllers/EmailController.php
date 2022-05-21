d73eecf9f7d45aa3caebd2bbc804f98d850e66be
mashely <ansherotimalnado10@gmail.com>
2022-05-21 16:32:57 +0300

    mail master credentials were added

--- sisilogistics/example-app/app/Http/Controllers/EmailController.php
+++ sisilogistics/example-app/app/Http/Controllers/EmailController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\support\facades\Mail;
+use App\Mail\MailMaster;
 
 class EmailController extends Controller
 {
-    //
+    public function sendEmail($title, $subject, $body, $email){
+  $credentials = [
+      'title'=>$title,
+      'subject'=>$subject,
+      'body'=>$body,
+      'email'=>$email,
+
+  ];
+
+  Mail::to($email)->send(new MailMaster($credentials));
+    }
 }
