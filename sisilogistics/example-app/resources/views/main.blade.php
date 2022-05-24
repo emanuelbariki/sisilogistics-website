1a2e3f0da52bde3dc8569fbf623fd93d95f0a0ac
mashely <ansherotimalnado10@gmail.com>
2022-05-24 12:41:14 +0300

    toolbar changes

--- sisilogistics/example-app/resources/views/main.blade.php
+++ sisilogistics/example-app/resources/views/main.blade.php
@@ -86,7 +86,8 @@
             <div class="row w-100">
               <div class="col-lg-12 pr-lg-0">
                 <ul class="nav navbar-nav ml-auto justify-content-end">
-                  <li class="nav-item active">
+
+                  <li class="nav-item ">
                     <a class="nav-link" href="/home">Home</a>
                   </li>
                   <li class="nav-item ">
